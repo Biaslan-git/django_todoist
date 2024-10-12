dd560624771385f6ba9f3aac3c40fbff25ebe70e
Biaslan-git <django.ppp@gmail.com>
2024-10-13 00:12:45 +0300

    Добавлены тесты для проверки загрузки стартовой страницы и содержания ссылки на страницу todoist

--- todoist/tests.py
+++ todoist/tests.py
@@ -1,2 +1,17 @@
 from django.test import TestCase
+from django.urls import reverse
+
+
+class StartPageTests(TestCase):
+    
+    start_page_url = '/'
+
+    def test_page_loads(self):
+        response = self.client.get(self.start_page_url)
+        self.assertEqual(response.status_code, 200) # type: ignore
+
+    def test_page_contains_todoist_url(self):
+        response = self.client.get(self.start_page_url)
+        self.assertContains(response, reverse('todoist'))
+
 
