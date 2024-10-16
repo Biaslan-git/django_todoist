6b23ae15b10bd1fd3bd8ed89fd65e8fa85c48694
Biaslan-git <django.ppp@gmail.com>
2024-10-16 16:58:20 +0300

    Начальная версия ветки master

--- todoist/urls.py
+++ todoist/urls.py
@@ -4,5 +4,4 @@ from .views import StartPageView, TodoistView
 
 urlpatterns = [
     path('', StartPageView.as_view(), name='start_page'),
-    path('todoist/',TodoistView.as_view(), name='todoist')
 ]
